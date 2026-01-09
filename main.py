3dd4cbab5aa2c7576e3ed4ff056f8b15905f1cc8
paulTroster <paultroster@gmail.com>
2026-01-09 12:58:19 +0100

    remove wind

--- main.py
+++ main.py
@@ -34,24 +34,6 @@ while running:
     # Get mouse position in order to let the arrows target it
     mouse_pos: tuple[int, int] = pygame.mouse.get_pos()
 
-    # Change wind direction randomly every 3-6 seconds
-    wind_change_timer += dt
-    if wind_change_timer > wind_change_interval:
-        wind_change_timer = 0
-        wind_change_interval = random.uniform(3, 6)
-        target_wind_direction = pygame.Vector2(
-            random.uniform(-1, 1), random.uniform(-1, 1)
-        ).normalize()
-        target_wind_strength = random.uniform(0.05, 0.2)
-    
-    # Smoothly interpolate wind direction and strength
-    lerp_factor = min(dt * 0.5, 1.0)  # Smooth transition speed
-    current_wind_direction = current_wind_direction.lerp(target_wind_direction, lerp_factor)
-    current_wind_strength += (target_wind_strength - current_wind_strength) * lerp_factor
-    
-    # Apply wind force
-    boidSystem.applyForceToAll(current_wind_direction * current_wind_strength)
-
     boidSystem.update(mouse_pos)
 
     keys = pygame.key.get_pressed()
