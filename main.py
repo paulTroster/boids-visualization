c93da02a9722c7f6ec4f209d91010a8c28add8d8
paulTroster <paultroster@gmail.com>
2026-01-20 15:30:51 +0100

    add slider example

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+import pygame_widgets
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
 from boid import BoidSystem
 
 # pygame setup
@@ -12,6 +15,9 @@ player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 boidSystem = BoidSystem(50, screen)
 
+slider = Slider(screen, 100, 100, 800, 40, min=0, max=99, step=1)
+output = TextBox(screen, 475, 200, 100, 50, fontSize=30)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -20,17 +26,21 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
 
+    pygame_widgets.update(screen)
+
     # Get mouse position in order to let the arrows target it
     mouse_pos: tuple[int, int] = pygame.mouse.get_pos()
 
     boidSystem.update(mouse_pos)
 
+    output.setText(str(slider.getValue()))
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_q]:
         running = False
 
-    if pygame.mouse.get_pressed()[0] == True:
-        boidSystem.applyForceToAll(pygame.Vector2(0, 0.1))  # Add gravitation
+    # if pygame.mouse.get_pressed()[0] == True:
+    #     boidSystem.applyForceToAll(pygame.Vector2(0, 0.1))  # Add gravitation
 
     # flip() the display to put your work on screen
     pygame.display.flip()
